a9626c6b935dd70e74d75431e410c77ea779a850
mkaye5 <mkaye@winsor.edu>
2017-07-21 09:51:30 -0400

    the facerec package

--- face/FaceRec.py
+++ face/FaceRec.py
@@ -1,14 +1,13 @@
-from camera import take_picture
-import matplotlib.pyplot as plt
+from camera import take_picture, use_camera
 from dlib_models import download_model, download_predictor, load_dlib_models
-
 download_model()
 download_predictor()
 from dlib_models import models
 import skimage.io as io
 import numpy as np
-from matplotlib.patches import Rectangle
 import warnings
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 load_dlib_models()
 face_detect = models["face detect"]
@@ -18,17 +17,29 @@ shape_predictor = models["shape predict"]
 
 class Face_Recognition:
     def __init__(self, upscale=1):
-        self.upscale = upscale  # number of times to upscale image before detecting
+        """ initializes an object of the Database class.
+            Parameters
+            ----------
+            upscale : (default 1) number of times to upscale image before detecting """
+        self.upscale = upscale
 
     def take_picture(self):
-        return take_picture()
+        """ takes a picture and returns the img array """
+        with use_camera() as camera:
+            pic = take_picture()
+        return pic
 
     def find_faces(self, pic, database):
-        """
-        Returns
-        -------
-        nothing
-        """
+        """ finds all of the faces in a picture and produces a picture with the faces highlighted
+            Parameters
+            ----------
+            pic : the image array
+            database : the database
+
+            Returns
+            ----------
+            desc_list : the list of the descriptor vectors that describe the faces in the image
+            name_list : the names matched to the faces """
         fig, ax = plt.subplots()
         ax.imshow(pic)
         desc_list = []
@@ -46,12 +57,15 @@ class Face_Recognition:
         return desc_list, name_list
 
     def compare_faces(self, database, desc):
-        """keys are names
-        iterate over database
-        find best match and assign a name
-        np.sqrt(np.sum((desc1 - desc2)**2))
-        """
-        # descript = a (128,) shape descriptor
+        """ Finds the best match face for a descriptor vector
+            Parameters
+            ----------
+            database : the database
+            desc : the descriptor vector produced by the picture; a (128,) shape descriptor
+
+            Returns
+            ----------
+            least_key : the best-matched name for the descriptor vector """
         least = 1
         least_key = ""
         for key in database.items():
@@ -66,6 +80,12 @@ class Face_Recognition:
             return least_key
 
     def update_faces(self, desc_list, name_list, database):
+        """ updates the face-vector based on a new descriptor vector
+            Parameters
+            ----------
+            desc_list : the descriptor vectors produced by the picture
+            name_list : the list of names that match with the descriptor vectors
+            database : the database """
         ans = input("Are the names and faces correctly matched? Unmatched faces will be added by your input. (y or n)")
         if ans == "y":
             for i in range(len(desc_list)):
@@ -85,12 +105,20 @@ class Face_Recognition:
             print("database not updated")
 
     def file_read(self, file_id):
+        """ reades a file on a computer
+            Parameters
+            ----------
+            file_id : the filepath to read the file from """
         img_array = io.imread(file_id)
         if (img_array.shape[2] == 4):
             img_array = img_array[:, :, :3]
         return img_array
 
     def name_faces_from_picture(self, database):
+        """ takes a picture and reads faces from that picture
+            Parameters
+            ----------
+            database : the database """
         pic = self.take_picture()
         desc, names = self.find_faces(pic, database)
         warnings.filterwarnings("ignore", ".*GUI is implemented.*")
@@ -98,10 +126,14 @@ class Face_Recognition:
             plt.pause(0.5)
         except Exception:
             pass
-
         self.update_faces(desc, names, database)
 
     def name_faces_from_file(self, file_id, database):
+        """ takes a file and reads faces from that picture
+            Parameters
+            ----------
+            file_id : the file to read from
+            database : the database """
         pic = self.file_read(file_id)
         desc, names = self.find_faces(pic, database)
         warnings.filterwarnings("ignore", ".*GUI is implemented.*")
@@ -112,6 +144,12 @@ class Face_Recognition:
         self.update_faces(desc, names, database)
 
     def add_new_from_file(self, file_id, database):
+        """ takes a file and ASSUMES that the face is a new one. Prompts the user to enter a new key for
+            the face
+            Parameters
+            ----------
+            file_id : the file to read from
+            database : the database """
         pic = self.file_read(file_id)
         desc, names = self.find_faces(pic, database)
         desc = desc[0]
@@ -124,6 +162,11 @@ class Face_Recognition:
         database.update(name, desc)
 
     def add_new_from_picture(self, database):
+        """ takes a picture and ASSUMES that the face is a new one. Prompts the user to enter a new key for
+            the face
+            Parameters
+            ----------
+            database : the database """
         pic = self.take_picture()
         desc = self.find_faces(pic, database)
         desc = desc[0]
