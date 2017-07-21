2498c2ec9bdd7b70ae6d699fb9856d6b3b4aad7e
Christine Zhao <czhao028@gmail.com>
2017-07-21 14:04:18 -0400

    Add Whisper code unfinished

--- database.py
+++ database.py
@@ -20,15 +20,19 @@ class Database():
             self.dict = {}
 
     def items(self):
+        """ returns the database's dictionary objects """
         return self.dict
     
     def get(self, key):
+        """ gets from the database's dictionary the obj based on the key """
         return self.dict[key]
     
     def update_user_image(self, key, val):
+        """ takes the average of the olf descriptor vector and the new one to update """
         self.dict[key] = (self.dict[key] + val)/2
         
     def update(self, key, val):
+        """ adds a new val in the dictionary """
         self.dict[key] = val
         
     def def_variation(self, var):
