91d299f14b1a130c920c5b92f80f0124f8643288
Megan Kaye <megan.kaye38@gmail.com>
2017-07-27 16:27:02 -0400

    megan's method added

---
+++ numberwon/alexa_skills/FaceRec.py
@@ -0,0 +1,105 @@
+from camera import take_picture, use_camera
+from dlib_models import download_model, download_predictor, load_dlib_models
+download_model()
+download_predictor()
+from dlib_models import models
+import skimage.io as io
+import numpy as np
+import warnings
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+load_dlib_models()
+face_detect = models["face detect"]
+face_rec_model = models["face rec"]
+shape_predictor = models["shape predict"]
+
+#idea: news buddy bases itself off of user preferences (users determined by face_rec)
+
+
+class Face_Recognition:
+    def __init__(self, upscale=1):
+        """ initializes an object of the Database class.
+            Parameters
+            ----------
+            upscale : (default 1) number of times to upscale image before detecting """
+        self.upscale = upscale
+
+    def take_picture(self):
+        """ takes a picture and returns the img array """
+        with use_camera() as camera:
+            pic = take_picture()
+        return pic
+
+    def find_faces(self, pic, database):
+        """ finds all of the faces in a picture and produces a picture with the faces highlighted
+            Parameters
+            ----------
+            pic : the image array
+            database : the database
+
+            Returns
+            ----------
+            desc_list : the list of the descriptor vectors that describe the faces in the image
+            name_list : the names matched to the faces """
+        desc_list = []
+        name_list = []
+        detections = list(face_detect(pic, self.upscale))
+        for i in range(len(detections)):
+            shape = shape_predictor(pic, detections[i])
+            desc = np.array(face_rec_model.compute_face_descriptor(pic, shape))
+            name = self.compare_faces(database, desc)
+            desc_list.append(desc)
+            name_list.append(name)
+        return desc_list, name_list, len(detections)
+
+    def compare_faces(self, database, desc):
+        """ Finds the best match face for a descriptor vector
+            Parameters
+            ----------
+            database : the database
+            desc : the descriptor vector produced by the picture; a (128,) shape descriptor
+
+            Returns
+            ----------
+            least_key : the best-matched name for the descriptor vector """
+        least = 1
+        least_key = ""
+        for key in database.items():
+            v = np.sqrt(np.sum((desc - database.items()[key]) ** 2))
+            if least > v:
+                least = v
+                least_key = key
+        if least > 0.45:
+            return "No match found"
+        else:
+            return least_key
+
+    def update_faces(self, desc_list, name_list, database):
+        """ updates the face-vector based on a new descriptor vector
+            Parameters
+            ----------
+            desc_list : the descriptor vectors produced by the picture
+            name_list : the list of names that match with the descriptor vectors
+            database : the database """
+        for i in range(len(desc_list)):
+            database.update_user_image(name_list[i], desc_list[i])
+        print("database updated")
+
+    def name_faces_from_picture(self, database):
+        """ takes a picture and reads faces from that picture
+            Parameters
+            ----------
+            database : the database """
+        pic = self.take_picture()
+        desc, names, num_faces = self.find_faces(pic, database)
+        return " ".join(names), names, desc
+
+    def add_face(self, database, name, desc):
+        """ takes a picture and ASSUMES that the face is a new one. Prompts the user to enter a new key for
+            the face
+            Parameters
+            ----------
+            database : the database """
+        desc = desc[0]
+        database.update(name, desc)
--- numberwon/alexa_skills/face_rec_skill.py
+++ numberwon/alexa_skills/face_rec_skill.py
@@ -4,45 +4,131 @@ import requests
 import time
 import unidecode
 import json
+from FaceRec import Face_Recognition
+from database import Database
+import numpy as np
+f = Face_Recognition()
+d = Database("goodfile.npy")
+num_faces = 0
+name = ""
+feature_vector = np.array([])
+global face_detect
+face_detect = False
+global start
+start = False
+global asking_about_names
+asking_about_names = False
+global asking_about_database
+asking_about_database = False
+global asked_about_database
+asked_about_database = False
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+
+
+#fix so that Echo can ask if ans is right, and then update it based off of what user inputs.
+
 @app.route('/')
 def homepage():
-    return "Hello"
+    return "Face Rec"
 
 @ask.launch
 def start_skill():
-    msg = "Hello. Would you like to hear the news?"
+    global face_detect
+    face_detect = False
+    global asking_about_names
+    asking_about_names = False
+    global asking_about_database
+    asking_about_database = False
+    global asked_about_database
+    asked_about_database = False
+    msg = "Hello. May I see your face?"
+    global start
+    start = True
     return question(msg)
 
-def get_headlines():
-    #return "Newsy news blah blah news news."
-    user_cred = {'user': 'cogworks2017',
-                 'passwd': 'beaverworks',
-                 'api_type': 'json'}
-    sess = requests.Session()
-    sess.headers.update({'User-Agent': 'cogworks'})
-    sess.post('https://www.reddit.com/api/login', data=user_cred)
-
-    url = 'https://reddit.com/r/worldnews/.json?limit=5'
-    html = sess.get(url)
-    data = json.loads(html.content.decode('utf-8'))
-    titles = [unidecode.unidecode(x['data']['title']) for x in data['data']['children']]
-    titles = "... ".join(titles)
-    return titles
+def take_picture():
+    names = f.name_faces_from_picture(d)
+    print(names)
+    return names
 
 @ask.intent("YesIntent")
-def share_headlines():
-    headlines = get_headlines()
-    headlines_msg = "The current world news headlines are, {}".format(headlines)
-    return statement(headlines_msg)
-
+def name_faces():
+    num_faces = 0
+    global face_detect
+    if not face_detect:
+        names, names_list, desc_vectors = take_picture()
+        global num_faces
+        num_faces = len(desc_vectors)
+        global feature_vector
+        feature_vector = desc_vectors
+        faces_msg = "I see {}. Is that correct?".format(names)
+        global face_detect
+        face_detect = True
+        global asking_about_database
+        asking_about_database = True
+        global start
+        start = False
+        return question(faces_msg)
+    global asking_about_names
+    if asking_about_names:
+        msg = "Adding {} to the database.".format(name)
+        f.add_face(d, name, feature_vector)
+        #d.save_obj("goodfile.npy")
+        global asking_about_names
+        asking_about_names = False
+        return statement(msg)
+    global asking_about_database
+    if asking_about_database:
+        msg = "Would you like me to update the database?"
+        asking_about_database = False
+        global asked_about_database
+        asked_about_database = True
+        return question(msg)
+    else:
+        state = "Great. Optimizing database based on correct results."
+        global feature_vector
+        global names_list
+        global d
+        f.update_faces(feature_vector, names_list, d)
+        #d.save_obj("goodfile.npy")
+        return statement(state)
+    
 @ask.intent("NoIntent")
 def no_intent():
-    msg = "Ok, thanks. Have a nice day."
-    return statement(msg)
+    global start
+    if start:
+        print(start)
+        msg = "Ok, thanks. Have a nice day."
+        return statement(msg)
+    global asking_about_names
+    if asking_about_names:
+        msg = "My apologies. Please say the correct name again."
+        return question(msg)
+    global asked_about_database
+    if asked_about_database:
+        msg = "Ok, thanks. Have a nice day."
+        asked_about_database = False
+        return statement(msg)
+    else:
+        global num_faces
+        if num_faces > 1:
+            msg = "My apologies. Please re-take the picture with only the unidentified person."
+            return statement(msg)
+        else:
+            msg = "Please say the correct name."
+            asking_about_names = True
+            return question(msg)
 
+@ask.intent("Melanie")
+def name_intent(nameslot):
+    print("name: " + str(nameslot))
+    global name
+    name = nameslot
+    msg = "Is {} correct?".format(nameslot)
+    return question(msg)
+    
 if __name__ == '__main__':
     app.run(debug=True)
