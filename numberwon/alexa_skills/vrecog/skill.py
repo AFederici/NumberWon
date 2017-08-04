1a90d2485d966c92f9a67be433714b17fcb46296
myh1000 <myh1000@gmail.com>
2017-08-04 15:59:35 -0400

    vrecog

--- numberwon/alexa_skills/vrecog/skill.py
+++ numberwon/alexa_skills/vrecog/skill.py
@@ -2,7 +2,7 @@ import record
 import formataudio
 import speaker_classifier_tflearn
 
-record.record_to_file("person_name") #create data
+record.record_to_file("personname") #create data
 speaker_classifier_tflearn.train() #retrain
 speaker_classifier_tflearn.loadmodel() #load/reload model
 
