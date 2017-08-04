b169ed6f2581e09f12dfb665a3a7b8b5766b59cb
myh1000 <myh1000@gmail.com>
2017-08-04 16:22:35 -0400

    vrecog

--- numberwon/alexa_skills/vrecog/skill.py
+++ numberwon/alexa_skills/vrecog/skill.py
@@ -5,7 +5,7 @@ import speaker_classifier_tflearn
 record.record_to_file("personname") #create data
 speaker_classifier_tflearn.train() #retrain
 import time
-time.sleep(2)
+time.sleep(5)
 speaker_classifier_tflearn.loadmodel() #load/reload model
 record.record_to_file("temp",train=False) #create data
 person = speaker_classifier_tflearn.test("temp.wav.ig") #call whenevr want to classify
