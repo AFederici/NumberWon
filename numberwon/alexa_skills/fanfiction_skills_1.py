7a6b73979894aa5d832b3265372afb42d3cd0004
Christine Zhao <czhao028@gmail.com>
2017-08-04 12:07:19 -0400

    fixed up fanfiction_skills

--- numberwon/alexa_skills/fanfiction_skills_1.py
+++ numberwon/alexa_skills/fanfiction_skills_1.py
@@ -5,6 +5,7 @@ import time
 import unidecode
 import json
 import numpy as np
+import html2text
 
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -25,11 +26,16 @@ profiles = [val.find_user_preferences(key) for key, val in d.dict.items() if "fa
 together.extend(itertools.chain.from_iterable(profiles))
 #together is a list with all the possible fanfiction preferences: NO elements should be "None"
 
+#MEGAN PLEASE DEFIND SAVE_PATH as '.../NumberWon/numberwon/alexa_skills/fanfiction_files
+save_path = #MEGAN INSERT HERE
 
+import sys
 for element in together:
     sys.path.insert(0, save_path)
     fanfiction_dict = f.find_fanfiction(element)
-    f.ultimate_function(element, fanfiction_dict)
+    a = open(element + '.txt', 'w')
+    a.write(fanfiction_dict[element])
+    a.close()
 
 @app.route('/')
 def homepage():
@@ -50,28 +56,33 @@ def start_skill():
 
 @ask.intent("AMAZON.YesIntent")
 def yes_intent():
-    msg = "Pulling up fanfiction for " + session.attributes["Current_User"]
-    session.attributes["Current_User"] = session.attributes["Current_User"].lower()
-
-    p = d.dict[session.attributes["Current_User"]]
-    listing = tuple(p.get_preferences_by_user(session.attributes["Current_User"], key) for key, val in p.pref_dict if "fan" in key)
-
-    print("preferences: ", listing)
-    content = ""
-    for term in listing:
-        with open(term + ".txt", "r") as z:
-            t = z.read()
-            print("first sentence of files :", t[:30])
-            content += term.title() + 'Fanfiction: \n' + t + "\n\n"
-
-    return statement(msg) \
-        .simple_card(title='Generated FanFic', content=content)
+    msg = "Ok " + session.attributes["Current_User"] +  ". How many characters of fanfiction would you like to generate for each topic?"
+    return question(msg)
 
 @ask.intent("AMAZON.NoIntent")
 def no_intent():
     msg = "No problem. Have a nice day."
     return question(msg)
 
+@ask.intent("CharacterIntent")
+def char_intent(number): #number type: AMAZON.NUMBER
+    session.attributes["Current_User"] = session.attributes["Current_User"].lower()
+    p = d.dict[session.attributes["Current_User"]]
+    listing = tuple(p.get_preferences_by_user(session.attributes["Current_User"], key) for key, val in p.pref_dict if "fan" in key)
+
+    print("preferences: ", listing)
+    content = ""
+
+    for term in listing:
+        with open(term + ".txt", "r") as z:
+            value = str(z.read())
+            lm = f.train_lm(value, 13)
+            text = html2text.html2text(f.generate_text(lm, 13, number))
+            text = text[:text.rfind(".") + 1]
+            content += term.title() + 'Fanfiction: \n' + value + "\n\n"
+
+    msg = "Pulling up fanfiction for " + session.attributes["Current_User"]
+    return statement(msg).simple_card(title='Generated FanFic', content=content)
 
 if __name__ == '__main__':
     app.run(debug=True)
