cb0df2cf31f4bc8fe2692ae2ec50348b6caa36cd
myh1000 <myh1000@gmail.com>
2017-08-05 16:00:58 -0400

    vrecog

--- numberwon/alexa_skills/vrecog/speech_data.py
+++ numberwon/alexa_skills/vrecog/speech_data.py
@@ -14,17 +14,9 @@ import numpy as np
 import scipy
 import skimage.io  # scikit-image
 
-try:
-	import librosa
-except:
-	print("pip install librosa ; if you want mfcc_batch_generator")
-# import extensions as xx
 from random import shuffle
-try:
-	from six.moves import urllib
-	from six.moves import xrange  # pylint: disable=redefined-builtin
-except:
-	pass # fuck 2to3
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
 
 # TRAIN_INDEX='train_words_index.txt'
 # TEST_INDEX='test_words_index.txt'
@@ -72,92 +64,12 @@ offset = 64  # starting with characters
 max_word_length = 20
 terminal_symbol = 0
 
-def pad(vec, pad_to=max_word_length, one_hot=False,paddy=terminal_symbol):
-	for i in range(0, pad_to - len(vec)):
-		if one_hot:
-			vec.append([paddy] * num_characters)
-		else:
-			vec.append(paddy)
-	return vec
-
-def char_to_class(c):
-	return (ord(c) - offset) % num_characters
-
-def string_to_int_word(word, pad_to):
-	z = map(char_to_class, word)
-	z = list(z)
-	z = pad(z)
-	return z
-
-class SparseLabels:
-	def __init__(labels):
-		labels.indices = {}
-		labels.values = []
-
-	def shape(self):
-		return (len(self.indices),len(self.values))
-
-# labels: An `int32` `SparseTensor`.
-# labels.indices[i, :] == [b, t] means `labels.values[i]` stores the id for (batch b, time t).
-# labels.values[i]` must take on values in `[0, num_labels)`.
-def sparse_labels(vec):
-	labels = SparseLabels()
-	b=0
-	for lab in vec:
-		t=0
-		for c in lab:
-			labels.indices[b, t] = len(labels.values)
-			labels.values.append(char_to_class(c))
-			# labels.values[i] = char_to_class(c)
-			t += 1
-		b += 1
-	return labels
-
-
-
-def progresshook(blocknum, blocksize, totalsize):
-		readsofar = blocknum * blocksize
-		if totalsize > 0:
-				percent = readsofar * 1e2 / totalsize
-				s = "\r%5.1f%% %*d / %d" % (
-						percent, len(str(totalsize)), readsofar, totalsize)
-				sys.stderr.write(s)
-				if readsofar >= totalsize: # near the end
-						sys.stderr.write("\n")
-		else: # total size is unknown
-				sys.stderr.write("read %d\n" % (readsofar,))
-
-def maybe_download(file, work_directory=DATA_DIR):
-	"""Download the data from Pannous's website, unless it's already here."""
-	print("Looking for data %s in %s"%(file,work_directory))
-	if not os.path.exists(work_directory):
-		os.mkdir(work_directory)
-	filepath = os.path.join(work_directory, re.sub('.*\/','',file))
-	if not os.path.exists(filepath):
-		if not file.startswith("http"): url_filename = SOURCE_URL + file
-		else: url_filename=file
-		print('Downloading from %s to %s' % (url_filename, filepath))
-		filepath, _ = urllib.request.urlretrieve(url_filename, filepath,progresshook)
-		statinfo = os.stat(filepath)
-		print('Successfully downloaded', file, statinfo.st_size, 'bytes.')
-		# os.system('ln -s '+work_directory)
-	if os.path.exists(filepath):
-		print('Extracting %s to %s' % ( filepath, work_directory))
-		os.system('tar xf '+filepath+" -C "+work_directory)
-		print('Data ready!')
-	return filepath.replace(".tar","")
-
-def spectro_batch(batch_size=10):
-	return spectro_batch_generator(batch_size)
-
-def speaker(filename):  # vom Dateinamen
+def speaker(filename):
 	# if not "_" in file:
 	#   return "Unknown"
 	return filename.split("_")[1].lower()
 
 def get_speakers(path=pcm_path):
-	# maybe_download(Source.DIGIT_SPECTROS)
-	maybe_download(Source.DIGIT_WAVES)
 	files = os.listdir(path)
 	def nobad(name):
 		return "_" in name and not "." in name.split("_")[1] and not 'DS_Store' in name
@@ -189,11 +101,9 @@ def load_wav_file(name):
 
 # only apply to a subset of all images at one time
 def wave_batch_generator(batch_size=10,source=Source.DIGIT_WAVES,target=Target.digits,path='data/spoken_numbers_pcm/'): #speaker
-	maybe_download(source, DATA_DIR)
 	if target == Target.speaker: speakers=get_speakers(path)
 	batch_waves = []
 	labels = []
-	# input_width=CHUNK*6 # wow, big!!
 	import glob
 	files = glob.glob(path+'*.wav')
 	# print(files)
@@ -202,9 +112,7 @@ def wave_batch_generator(batch_size=10,source=Source.DIGIT_WAVES,target=Target.d
 		# print("loaded batch of %d files" % len(files))
 		for wav in files:
 			if not wav.endswith(".wav"):continue
-			if target==Target.digits: labels.append(dense_to_one_hot(int(wav[0])))
-			elif target==Target.speaker: labels.append(one_hot_from_item(speaker(wav), speakers))
-			elif target==Target.first_letter:  label=dense_to_one_hot((ord(wav[0]) - 48) % 32,32)
+			if target==Target.speaker: labels.append(one_hot_from_item(speaker(wav), speakers))
 			else: raise Exception("todo : Target.word label!")
 			chunk = load_wav_file(wav)
 			batch_waves.append(chunk)
@@ -213,97 +121,6 @@ def wave_batch_generator(batch_size=10,source=Source.DIGIT_WAVES,target=Target.d
 				yield batch_waves, labels
 				batch_waves = []  # Reset for next batch
 				labels = []
-
-class DataSet(object):
-
-	def __init__(self, images, labels, fake_data=False, one_hot=False, load=False):
-		"""Construct a DataSet. one_hot arg is used only if fake_data is true."""
-		if fake_data:
-			self._num_examples = 10000
-			self.one_hot = one_hot
-		else:
-			num = len(images)
-			assert num == len(labels), ('images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
-			print("len(images) %d" % num)
-			self._num_examples = num
-		self.cache={}
-		self._image_names = numpy.array(images)
-		self._labels = labels
-		self._epochs_completed = 0
-		self._index_in_epoch = 0
-		self._images=[]
-		if load: # Otherwise loaded on demand
-			self._images=self.load(self._image_names)
-
-	@property
-	def images(self):
-		return self._images
-
-	@property
-	def image_names(self):
-		return self._image_names
-
-	@property
-	def labels(self):
-		return self._labels
-
-	@property
-	def num_examples(self):
-		return self._num_examples
-
-	@property
-	def epochs_completed(self):
-		return self._epochs_completed
-
-	# only apply to a subset of all images at one time
-	def load(self,image_names):
-		print("loading %d images"%len(image_names))
-		return list(map(self.load_image,image_names)) # python3 map object WTF
-
-	def load_image(self,image_name):
-		if image_name in self.cache:
-				return self.cache[image_name]
-		else:
-			image = skimage.io.imread(DATA_DIR+ image_name).astype(numpy.float32)
-			# images = numpy.multiply(images, 1.0 / 255.0)
-			self.cache[image_name]=image
-			return image
-
-
-	def next_batch(self, batch_size, fake_data=False):
-		"""Return the next `batch_size` examples from this data set."""
-		if fake_data:
-			fake_image = [1] * width * height
-			if self.one_hot:
-				fake_label = [1] + [0] * 9
-			else:
-				fake_label = 0
-			return [fake_image for _ in xrange(batch_size)], [
-					fake_label for _ in xrange(batch_size)]
-		start = self._index_in_epoch
-		self._index_in_epoch += batch_size
-		if self._index_in_epoch > self._num_examples:
-			# Finished epoch
-			self._epochs_completed += 1
-			# Shuffle the data
-			perm = numpy.arange(self._num_examples)
-			numpy.random.shuffle(perm)
-			# self._images = self._images[perm]
-			self._image_names = self._image_names[perm]
-			self._labels = self._labels[perm]
-			# Start next epoch
-			start = 0
-			self._index_in_epoch = batch_size
-			assert batch_size <= self._num_examples
-		end = self._index_in_epoch
-		return self.load(self._image_names[start:end]), self._labels[start:end]
-
-
-# multi-label
-def dense_to_some_hot(labels_dense, num_classes=140):
-	"""Convert class labels from int vectors to many-hot vectors!"""
-	raise "TODO dense_to_some_hot"
-
 
 def one_hot_to_item(hot, items):
 	i=np.argmax(hot)
@@ -316,51 +133,3 @@ def one_hot_from_item(item, items):
 	i=items.index(item)
 	x[i]=1
 	return x
-
-def extract_labels(names_file,train, one_hot):
-	labels=[]
-	for line in open(names_file).readlines():
-		image_file,image_label = line.split("\t")
-		labels.append(image_label)
-	if one_hot:
-			return dense_to_one_hot(labels)
-	return labels
-
-def extract_images(names_file,train):
-	image_files=[]
-	for line in open(names_file).readlines():
-		image_file,image_label = line.split("\t")
-		image_files.append(image_file)
-	return image_files
-
-
-def read_data_sets(train_dir,source_data=Source.NUMBER_IMAGES, fake_data=False, one_hot=True):
-	class DataSets(object):
-		pass
-	data_sets = DataSets()
-	if fake_data:
-		data_sets.train = DataSet([], [], fake_data=True, one_hot=one_hot)
-		data_sets.validation = DataSet([], [], fake_data=True, one_hot=one_hot)
-		data_sets.test = DataSet([], [], fake_data=True, one_hot=one_hot)
-		return data_sets
-	VALIDATION_SIZE = 2000
-	local_file = maybe_download(source_data, train_dir)
-	train_images = extract_images(TRAIN_INDEX,train=True)
-	train_labels = extract_labels(TRAIN_INDEX,train=True, one_hot=one_hot)
-	test_images = extract_images(TEST_INDEX,train=False)
-	test_labels = extract_labels(TEST_INDEX,train=False, one_hot=one_hot)
-	# train_images = train_images[:VALIDATION_SIZE]
-	# train_labels = train_labels[:VALIDATION_SIZE:]
-	# test_images = test_images[VALIDATION_SIZE:]
-	# test_labels = test_labels[VALIDATION_SIZE:]
-	data_sets.train = DataSet(train_images, train_labels , load=False)
-	data_sets.test = DataSet(test_images, test_labels, load=True)
-	# data_sets.validation = DataSet(validation_images, validation_labels, load=True)
-	return data_sets
-
-if __name__ == "__main__":
-	print("downloading speech datasets")
-	maybe_download( Source.DIGIT_SPECTROS)
-	maybe_download( Source.DIGIT_WAVES)
-	maybe_download( Source.NUMBER_IMAGES)
-	maybe_download( Source.NUMBER_WAVES)
