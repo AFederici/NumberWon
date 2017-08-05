6bfb90a507ae1b9b94a74d63b3ad4d9668be1b0a
myh1000 <myh1000@gmail.com>
2017-08-05 16:08:03 -0400

    vrecog

--- numberwon/alexa_skills/vrecogskill.py
+++ numberwon/alexa_skills/vrecogskill.py
@@ -3,8 +3,8 @@ import vrecog.record as record
 import vrecog.speaker_classifier_tflearn as speaker_classifier_tflearn
 """Miscellaneous testing for speaker recog"""
 
-# record.record_to_file("julie") #create data
-# speaker_classifier_tflearn.train() #retrain
+record.record_to_file("michael") #create data
+speaker_classifier_tflearn.train() #retrain
 
 record.record_to_file("temp",train=False) #create data
 person = speaker_classifier_tflearn.test("temp.wav.ig") #call whenevr want to classify
